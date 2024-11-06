6fb656b0ec34411dfc6b25b7355d335acccad011
alexoesmith <asmith@oomphworks.com>
2024-11-06 15:51:54 +0000

    Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  extends: ["../../layers/base"],
+
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4,
   },
